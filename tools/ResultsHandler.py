3bd495a44b319fca9955c1617420883d78d32d82
ChristopherLemon <lemoncp@outlook.com>
2018-06-23 18:04:35 +0100

    formatting

--- tools/ResultsHandler.py
+++ tools/ResultsHandler.py
@@ -1,4 +1,3 @@
-import io
 import os
 import re
 from tools.Utilities import natural_sort
@@ -7,18 +6,19 @@ from tempfile import mkstemp
 from shutil import move
 from os import remove, close
 
+
 def modify_process_ids(orig_pid, orig_file):
-# Replace process and thread ids with sets of sequential ids, starting from zero
+    # Replace process and thread ids with sets of sequential ids, starting from zero
     threads = {}
     process_id_regex = re.compile("((all|[0-9]+)/(all|[0-9]+))")
-    with open(orig_file,'r') as result:
+    with open(orig_file, 'r') as result:
         for line in result:
             match = re.search(process_id_regex, line)
             if match:
                 tid = match.group(3)
                 if tid not in threads:
                     threads[tid] = tid
-    sorted_threads = OrderedDict(natural_sort(threads.items(), key=lambda t: t[1]))
+    sorted_threads = OrderedDict(natural_sort(threads.items(), key=lambda th: th[1]))
     tids = OrderedDict()
     n = 0
     for t in sorted_threads:
@@ -28,16 +28,16 @@ def modify_process_ids(orig_pid, orig_file):
             tids[t] = str(n)
             n += 1
     fh, abs_path = mkstemp()
-    with open(abs_path,'wb') as new_file:  # Output file is binary
-        with open(orig_file,'r') as result:  # Input file is text
+    with open(abs_path, 'wb') as new_file:  # Output file is binary
+        with open(orig_file, 'r') as result:  # Input file is text
             for line in result:
                 match = re.search(process_id_regex, line)
                 if match:
                     pid = match.group(2)
                     tid = match.group(3)
-                    l = re.sub("/" + tid,"/" + tids[tid], line)
-                    l = re.sub(pid + "/",orig_pid + "/", l)
-                    new_file.write(l.encode())
+                    line = re.sub("/" + tid, "/" + tids[tid], line)
+                    line = re.sub(pid + "/", orig_pid + "/", line)
+                    new_file.write(line.encode())
                 else:
                     new_file.write(line.encode())
     close(fh)
@@ -46,18 +46,17 @@ def modify_process_ids(orig_pid, orig_file):
 
 
 def modify_system_wide_process_ids(orig_file):
-# Replace process and thread ids with sets of sequential ids, starting from zero
-# split data files for each host into separate files for each physical core
+    # Replace process and thread ids with sets of sequential ids, starting from zero
+    # split data files for each host into separate files for each physical core
     threads = []
-    host = re.findall("host(\d+)_",orig_file)[0][0]
+    host = re.findall("host(\d+)_", orig_file)[0][0]
     process_id_regex = re.compile("((all|[0-9]+)/(all|[0-9]+))")
     with open(orig_file, 'r') as result:
         for line in result:
             match = re.search(process_id_regex, line)
             if match:
-                pid = match.group(2)
                 tid = match.group(3)
-                if not tid in threads:
+                if tid not in threads:
                     threads.append(tid)
     sorted_threads = natural_sort(threads)
     tids = OrderedDict()
@@ -76,12 +75,12 @@ def modify_system_wide_process_ids(orig_file):
             if match:
                 pid = match.group(2)
                 tid = match.group(3)
-                l = re.sub("/" + tid, "/" + tids[tid], line)
+                line = re.sub("/" + tid, "/" + tids[tid], line)
                 new_file = re.sub("host" + host, "host" + host + "_proc" + pid, orig_file)
                 if new_file not in fs:
                     fs[new_file] = open(new_file, 'wb')
                     fs[new_file].write(last_time.encode())
-                fs[new_file].write(l.encode())
+                fs[new_file].write(line.encode())
             elif line[0:2] == "t=":
                 last_time = line
                 for f in fs:
@@ -90,8 +89,9 @@ def modify_system_wide_process_ids(orig_file):
         fs[f].close()
     remove(orig_file)
 
+
 def get_run_duration(stack_file):
-# read last line of stacks file to get the last time recorded. i.e. t=10.0
+    # read last line of stacks file to get the last time recorded. i.e. t=10.0
     fs = open(stack_file, 'rb')
     file_size = os.path.getsize(stack_file)
     if file_size < 1000:
@@ -108,25 +108,26 @@ def get_run_duration(stack_file):
 
 
 def replace_results_file(local_data, results_file, job_id):
-# Create new results file for system wide monitoring, with one file per core (for each event)
+    # Create new results file for system wide monitoring, with one file per core (for each event)
     fh, abs_path = mkstemp()
     with open(abs_path, 'wb') as new_results_file:
         with open(results_file, 'r') as result:
             for line in result:
                 if (re.match("event_counter", line) or
-                    re.match("time_interval", line) or
-                    re.match("cpu_id", line) or
-                    re.match("system_wide", line)): # copy run settings at start of results file
+                        re.match("time_interval", line) or
+                        re.match("cpu_id", line) or
+                        re.match("system_wide", line)):  # copy run settings at start of results file
                     new_results_file.write(line.encode())
-                else: # ignore original results files
+                else:  # ignore original results files
                     break
         for f in os.listdir(local_data):
-            if re.search(job_id + "_host(\d+)_proc",f): # populate new file with seperate results for each process
+            if re.search(job_id + "_host(\d+)_proc", f):  # populate new file with seperate results for each process
                 fname = f + "\n"
                 new_results_file.write(fname.encode())
     close(fh)
     remove(results_file)
     move(abs_path, results_file)
+
 
 def get_events(path, results_files):  # Read events from results files
     events = []
@@ -135,9 +136,9 @@ def get_events(path, results_files):  # Read events from results files
         with open(full_filename) as infile:
             for line in infile:
                 if not (re.match("event_counter", line) or
-                            re.match("time_interval", line) or
-                            re.match("cpu_id", line) or
-                            re.match("system_wide", line)):
+                        re.match("time_interval", line) or
+                        re.match("cpu_id", line) or
+                        re.match("system_wide", line)):
                     match = re.match("(.*proc(all|[0-9]+))_(.*)", line.strip())
                     event = match.group(3)
                     if event not in events:
@@ -145,7 +146,8 @@ def get_events(path, results_files):  # Read events from results files
     events = natural_sort(events)
     return events
 
-def get_processes(path, results_files): # Read processes from results files
+
+def get_processes(path, results_files):  # Read processes from results files
     processes = []
     for result_file in results_files:
         full_filename = os.path.join(path, result_file)
@@ -162,19 +164,20 @@ def get_processes(path, results_files): # Read processes from results files
     processes = natural_sort(processes)
     return processes
 
+
 def get_process_to_event_map(path, results_file):  # Get a process to event map, from a results file
     found = {}
     full_filename = os.path.join(path, results_file)
     with open(full_filename) as infile:
         for line in infile:
             if not (re.match("event_counter", line) or
-                        re.match("time_interval", line) or
-                        re.match("cpu_id", line) or
-                        re.match("system_wide", line)):
+                    re.match("time_interval", line) or
+                    re.match("cpu_id", line) or
+                    re.match("system_wide", line)):
                 match = re.match("(.*proc(all|[0-9]+))_(.*)", line.strip())
                 name = match.group(1)
                 event = match.group(3)
-                if re.search("trace", event): # skip trace events
+                if re.search("trace", event):  # skip trace events
                     continue
                 if name in found:
                     found[name].append(event)
@@ -182,7 +185,9 @@ def get_process_to_event_map(path, results_file):  # Get a process to event map,
                     found[name] = [event]
     return found
 
-def get_jobs(results_files):  # Read jobs from results file
+
+def get_jobs(results_files):
+    # Read jobs from results file
     jobs = []
     for result_file in results_files:
         job = result_file.partition(".results")[0]
@@ -190,8 +195,10 @@ def get_jobs(results_files):  # Read jobs from results file
     jobs = natural_sort(jobs)
     return jobs
 
+
 def get_job_name(result_file):
     return result_file.partition(".results")[0]
+
 
 def get_event_counters(path, results_files):
     event_counters = {}
@@ -209,6 +216,7 @@ def get_event_counters(path, results_files):
                     event_counter = match.group(2)
                     event_counters[job][raw_event] = int(event_counter)
     return event_counters
+
 
 def get_run_summary(path, results_files):
     run_durations = {}
@@ -249,8 +257,9 @@ def get_run_summary(path, results_files):
                         if run_number not in run_durations[job]:
                             run_durations[job][run_number] = t
                         else:
-                            run_durations[job][run_number] = max(run_durations[job][run_number],t)
+                            run_durations[job][run_number] = max(run_durations[job][run_number], t)
     return event_counters, run_numbers, run_durations, run_parameters
+
 
 def get_cpu(path, results_files):
     for result_file in results_files:
@@ -261,7 +270,9 @@ def get_cpu(path, results_files):
                     cpu = line.strip().partition(":")[2]
                     return cpu
 
-def get_results_info(path, results_files):  # Read processes from results file
+
+def get_results_info(path, results_files):
+    # Read processes from results file
     job_process_map = OrderedDict()
     processes = {}
     jobs = []
@@ -274,9 +285,9 @@ def get_results_info(path, results_files):  # Read processes from results file
         with open(full_filename) as infile:
             for line in infile:
                 if not (re.match("event_counter", line) or
-                            re.match("time_interval", line) or
-                            re.match("cpu_id", line) or
-                            re.match("system_wide", line)):
+                        re.match("time_interval", line) or
+                        re.match("cpu_id", line) or
+                        re.match("system_wide", line)):
                     match = re.match("(.*proc(all|[0-9]+))_(.*)", line.strip())
                     job_process = match.group(1)
                     event = match.group(3)
