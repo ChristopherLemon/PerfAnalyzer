8c37138da5bc9a4b39f41585069bfc84f700b0fb
ChristopherLemon <lemoncp@outlook.com>
2018-11-26 18:10:45 +0000

    add missing files

---
+++ plotting/SourceCode.py
@@ -0,0 +1,124 @@
+import os
+from tools.Utilities import round_sig
+
+def get_lines(frame, hpc_results):
+    frames = hpc_results.get_frames()
+    if frame in frames:
+        info = frames[frame]
+        file = os.path.join(hpc_results.results_dir, info[0])
+        line_num = int(info[1])
+        text = {"lines": [], "focus": line_num}
+        with open(file) as f:
+            for line in f.readlines():
+                text["lines"].append(line)
+    else:
+        text = {"lines": ["No Data"], "focus": 0}
+    return text
+
+
+def get_percentages(stacks_data, frame, hpc_results):
+    frames = hpc_results.get_frames()
+    if frame in frames:
+        info = frames[frame]
+    else:
+        return {}, {}
+    current_file = os.path.join(hpc_results.results_dir, info[0])
+    line_counts = {}
+    base_case_id = stacks_data.get_base_case_id()
+    if base_case_id.event_type == "original":
+        x_data = stacks_data.get_original_event_stack_data(base_case_id)
+        total = 0.0
+        for stack in x_data:
+            node = stack.rpartition(";")[2]
+            if node in frames:
+                info = frames[node]
+                file = os.path.join(hpc_results.results_dir, info[0])
+                if file == current_file:
+                    count = x_data[stack]
+                    line_num = int(info[1])
+                    line_counts[line_num] = count
+            total += float(x_data[stack])
+        line_percentages = {}
+        for line_num in line_counts:
+            line_percentages[line_num] = 100.0 * float(line_counts[line_num]) / total
+        return line_percentages, line_counts
+    else:
+        x_data, y_data = stacks_data.get_custom_event_ratio_stack_data(base_case_id)
+        total = 0.0
+        ratios = {}
+        for stack in x_data:
+            node = stack.rpartition(";")[2]
+            if node in frames:
+                info = frames[node]
+                file = os.path.join(hpc_results.results_dir, info[0])
+                if file == current_file:
+                    if stack in y_data:
+                        count1 = x_data[stack]
+                        count2 = y_data[stack]
+                        line_num = int(info[1])
+                        if float(count1) >= 0.1:
+                            ratios[line_num] = float(count2) / float(count1)
+                            line_counts[line_num] = count1
+            total += float(x_data[stack])
+        line_percentages = {}
+        for line_num in line_counts:
+            line_percentages[line_num] = 100.0 * float(line_counts[line_num]) / total
+        return line_percentages, ratios
+
+def generate_empty_table():
+    table_html = []
+    table_html.append("<table>")
+    table_html.append("<thead>")
+    table_html.append("<tr>")
+    table_html += ["<th>Line</th>", "<th>Source Code</th>", "<th>Event Count</th>", "<th>Percentage of Total</th>"]
+    table_html.append("</tr>")
+    table_html.append("</thead>")
+    table_html.append("<tr bgcolor=\"white\">")
+    table_html.append("<td>" + str(1).ljust(5) + ": " + "</td>")
+    table_html.append("<td><pre> No Data </pre></td>")
+    table_html.append("<td> - </td>")
+    table_html.append("<td> - </td>")
+    table_html.append("</tr>")
+    table_html.append("</table>")
+    table = "".join(table_html)
+    return table, "0"
+
+
+def generate_source_code_table(stacks_data, frame, hpc_results):
+    base_case_id = stacks_data.get_base_case_id()
+    source_lines = get_lines(frame, hpc_results)
+    line_percentages, line_counts = get_percentages(stacks_data, frame, hpc_results)
+    table_html = []
+    table_html.append("<table>")
+    table_html.append("<thead>")
+    table_html.append("<tr>")
+    if base_case_id.event_type == "original":
+        table_html += ["<th>Line</th>", "<th>Source Code</th>", "<th>Event Count</th>", "<th>Percentage of Total</th>"]
+    else:
+        table_html += ["<th>Line</th>", "<th>Source Code</th>", "<th>Event Ratio</th>", "<th>Percentage of Total</th>"]
+    table_html.append("</tr>")
+    table_html.append("</thead>")
+    max_len = 1
+    for line in source_lines["lines"]:
+        max_len = max(max_len, len(line))
+    line_num = 0
+    for line in source_lines["lines"]:
+        line_num += 1
+        if line_percentages and line_num in line_percentages:
+            count = str(line_counts[line_num])
+            pc = str(round_sig(line_percentages[line_num], 4))
+        else:
+            count = "-"
+            pc = "-"
+        if line_num == source_lines["focus"]:
+            table_html.append("<tr bgcolor=\"green\">")
+        else:
+            table_html.append("<tr bgcolor=\"white\">")
+        table_html.append("<td>" + str(line_num).ljust(5) + ": " + "</td>")
+        table_html.append("<td><pre>" + line.rstrip().ljust(max_len) + "</pre></td>")
+        table_html.append("<td>" + count.ljust(16) + "</td>")
+        table_html.append("<td>" + pc.ljust(5) + "</td>")
+        table_html.append("</tr>")
+    table_html.append("</table>")
+    table = "".join(table_html)
+    return table.replace("&", "&amp;"), source_lines["focus"]
---
+++ tools/HPCExperiment.py
@@ -0,0 +1,316 @@
+import re
+import sys
+import pathlib
+import os
+import fnmatch
+from pathlib import Path
+from lxml import etree
+from plotting.ColourMaps import get_top_ten_colours
+
+
+def is_HPC_result(results_file):
+    filename = re.sub("\.results", ".frames", results_file)
+    return os.path.exists(filename)
+
+
+class HPCExperimentHandler:
+
+    def __init__(self, results_dir, experiment):
+        self.results_dir = results_dir
+        self.experiment_file = experiment
+        self.job_id = pathlib.Path(experiment).parent.name
+
+    def create_results(self, include_loops=True, include_statements=False ):
+        self.results_file = os.path.join(self.results_dir, self.job_id) + ".results"
+        f = open(self.results_file, "wb")
+        f.write("cpu_id:General\n".encode())
+        f.write("time_interval:1.00\n".encode())
+        hpc_experiment = HPCExperiment(self.results_dir, self.job_id)
+        hpc_experiment.read_experiment(self.experiment_file, include_loops, include_statements)
+        hpc_experiment.process_experiment()
+        metrics = hpc_experiment.get_metrics()
+        results = hpc_experiment.get_results()
+        for metric in metrics:
+            info = "event_counter-" + metric.lower() + ":run-" + str(1) + ":" + str(metrics[metric]) + "\n"
+            f.write(info.encode())
+        for result in results:
+            out = result + "\n"
+            f.write(out.encode())
+        f.close()
+        return self.results_file
+
+    def get_results_file_name(self):
+        return self.results_file
+
+    def get_experiment_file_name(self):
+        return self.experiment_file
+
+
+class HPCResultsHandler:
+
+    def __init__(self, results_dir, results_file):
+        self.results_dir = results_dir
+        self.job_id = pathlib.Path(results_file).stem
+        self.frames_file = os.path.join(self.results_dir, self.job_id) + ".frames"
+        self.results_file = results_file
+        self.frames = self.load_frames()
+
+    def load_frames(self):
+        frames = {}
+        with open(self.frames_file) as f:
+            for line in f.readlines():
+                match = re.match("(.*) (.*)@([0-9]+)\n", line)
+                if match:
+                    frame = match.group(1)
+                    file = match.group(2)
+                    line = match.group(3)
+                    frames[frame] = (file, line)
+        return frames
+
+    def get_frames(self):
+        return self.frames
+
+    def get_job_id(self):
+        return self.job_id
+
+
+class HPCExperiment:
+
+    def __init__(self, results_dir, job_id):
+        self.results_dir = results_dir
+        self.job_id = job_id
+        self.file_map = {}
+        self.colormap = {}
+        self.procedure_map = {}
+        self.metric_to_results_map = {}
+
+    def read_experiment(self, experiment_file, include_loops=True, include_statements=False):
+        self.experiment_file = experiment_file
+        self.include_loops = include_loops
+        self.include_statements = include_statements
+        self.experiment_tree = etree.parse(experiment_file)
+        self.procedure_map = self.get_procedure_names(self.experiment_tree)
+        self.file_map = self.get_file_names(self.experiment_tree)
+        self.frames = {}
+
+    def process_experiment(self):
+        self.metrics = {}
+        self.results_files = {}
+        self.header = self.find_header()
+        self.metric_info = self.find_metric_info()
+        colors = get_top_ten_colours()
+        previous_stack_trace = {}
+        current_stack_trace = {}
+        self.color_map = {}
+        current_count = {}
+        current_stack = [""]
+        walk_experiment = self.experiment_tree.getiterator()
+        for elt in walk_experiment:
+            node_level = self.get_node_level(elt)
+            if node_level < len(current_stack):
+                n = len(current_stack) - node_level
+                del current_stack[-n:]
+            if elt.tag == 'PF':
+                name = elt.attrib['n']
+                p_name = self.procedure_map[name]
+                if node_level == len(current_stack):
+                    current_stack[-1] =  p_name
+                else:
+                    current_stack.append(p_name)
+            elif self.include_loops and elt.tag == 'L':
+                line = elt.attrib['l']
+                p_name = self.get_procedure_name(elt, self.procedure_map)
+                id = "Loop@" + str(line) + "@" + p_name
+                self.color_map[id] = colors[8]
+                if node_level == len(current_stack):
+                    current_stack[-1] =  id
+                else:
+                    current_stack.append(id)
+            elif self.include_statements and elt.tag == 'S':
+                line = elt.attrib['l']
+                p_name = self.get_procedure_name(elt, self.procedure_map)
+                id = "Line@" + str(line) + "@" + p_name
+                self.color_map[id] = colors[0]
+                if node_level == len(current_stack):
+                    current_stack[-1] =  id
+                else:
+                    current_stack.append(id)
+            elif elt.tag == 'M':
+                n = elt.attrib['n']
+                if n in self.metric_info:
+                    stack_trace = ";".join(current_stack[1:])
+                    stack_trace = re.sub(" ", "", stack_trace)
+                    metric_info = self.metric_info[n]
+                    metric = metric_info[0]
+                    process = metric_info[1]
+                    thread = metric_info[2]
+                    period = metric_info[3]
+                    id = (metric, process, thread)
+                    if id not in current_count:
+                        current_count[id] = 0
+                        previous_stack_trace[id] = stack_trace
+                    current_stack_trace[id] = stack_trace
+                    if current_count[id] > 0 and current_stack_trace[id] != previous_stack_trace[id]:
+                        if metric not in self.metrics:
+                            self.metrics[metric] = period
+                        out = "{}-{}/{};{} {}\n".format(self.header, process, thread, previous_stack_trace[id], str(current_count[id]))
+                        frame = current_stack[-1]
+                        frame = re.sub(" ", "", frame)
+                        self.frames[frame] = self.unwind_frame_details(elt, self.file_map)
+                        filename = self.get_results_file_name(metric, process)
+                        if filename not in self.results_files:
+                            self.results_files[filename] = open(filename, 'wb')
+                            self.results_files[filename].write("t=0.00\n".encode())
+                        self.results_files[filename].write(out.encode())
+                        current_count[id] = 0
+                        previous_stack_trace[id] = current_stack_trace[id]
+                    total = int(round(float(elt.attrib['v']) / float(period)))
+                    current_count[id] += total
+        for id in current_stack_trace:
+            metric = id[0]
+            process = id[1]
+            thread = id[2]
+            out = "{}-{}/{};{} {}\n".format(self.header, process, thread, current_stack_trace[id], str(current_count[id]))
+            filename = self.get_results_file_name(metric, process)
+            if filename not in self.results_files:
+                self.results_files[filename] = open(filename, 'wb')
+                self.results_files[filename].write(out.encode())
+        for filename in self.results_files:
+            self.results_files[filename].write("t=1.00\n".encode())
+            self.results_files[filename].close()
+        self.write_file_map()
+
+    def write_file_map(self):
+        filename = os.path.join(self.results_dir, self.job_id) + ".frames"
+        f = open(filename, 'wb')
+        for frame in self.frames:
+            info = self.frames[frame]
+            file = info[0]
+            line = info[1]
+            database_location = os.path.dirname(self.experiment_file)
+            path = os.path.relpath(os.path.join(database_location, Path(file)), self.results_dir)
+            out = "{} {}@{}\n".format(frame, path, line)
+            f.write(out.encode())
+        f.close
+
+    def get_color_map(self):
+        return self.color_map
+
+    def find_header(self):
+        root = self.experiment_tree.getroot()
+        return root.find('.//Header').attrib['n']
+
+    def get_header(self):
+        return self.header
+
+    def get_results_file_name(self, metric, process):
+        name = self.job_id + "_proc" + str(process) + "_" + metric.lower()
+        results_file = os.path.join(self.results_dir, name)
+        return results_file
+
+    def get_metric_info(self):
+        return self.get_metric_info
+
+    def get_results(self):
+        results = set()
+        for result in self.results_files:
+            file = os.path.basename(result)
+            results.add(file)
+        return results
+
+    def get_metrics(self):
+        return self.metrics
+
+    def find_metric_info(self):
+        root = self.experiment_tree.getroot()
+        pt = root.find('.//MetricTable')
+        metric_info = {}
+        for f in pt.findall('Metric'):
+            it = f.getiterator()
+            for elt in it:
+                if "n" in elt.attrib:
+                    n = elt.attrib["n"]
+                    if n == "period":
+                        period = elt.attrib["v"]
+            n = f.attrib['n']
+            i = f.attrib['i']
+            match = re.match("(.*)\.\[([0-9]+),([0-9]+)\]", n)
+            if match:
+                metric = match.group(1)
+                process = match.group(2)
+                thread = match.group(3)
+                metric_info[i] = (metric, process, thread, period)
+                filename = self.get_results_file_name(metric, process)
+                self.metric_to_results_map[i] = filename
+        return metric_info
+
+    def get_file_names(self, experiment_tree):
+        file_map = {}
+        root = experiment_tree.getroot()
+        pt = root.find('.//FileTable')
+        for f in pt.findall('File'):
+            i = f.attrib['i']
+            n = f.attrib['n']
+            file_map[i] = n
+        return file_map
+
+    def get_procedure_names(self, experiment_tree):
+        procedure_map = {}
+        root = experiment_tree.getroot()
+        pt = root.find('.//ProcedureTable')
+        for f in pt.findall('Procedure'):
+            i = f.attrib['i']
+            n = f.attrib['n']
+            procedure_map[i] = n
+        return procedure_map
+
+    def get_procedure_name(self, node, procedure_map):
+        nd = node
+        while nd is not None:
+            if nd.tag == "PF":
+                name = nd.attrib['n']
+                return procedure_map[name]
+            nd = nd.getparent()
+
+    def get_file_info(self, frame):
+        return self.frames[frame]
+
+    def unwind_frame_details(self, node, file_map):
+        nd = node
+        line = None
+        file = None
+        while nd is not None:
+            if nd.tag == "PF":
+                f = nd.attrib['f']
+                file = file_map[f]
+                if not line:
+                    line = nd.attrib['l']
+            elif self.include_loops and nd.tag == "L":
+                f = nd.attrib['f']
+                file = file_map[f]
+                if not line:
+                    line = nd.attrib['l']
+            elif self.include_statements and nd.tag == "S":
+                line = nd.attrib['l']
+            if line and file:
+                return (file, line)
+            nd = nd.getparent()
+
+    def get_node_level(self, node):
+        d = 1
+        nd = node
+        while nd is not None:
+            if nd.tag == "PF":
+                d += 1
+            elif self.include_loops and nd.tag == "L":
+                d += 1
+            elif self.include_statements and nd.tag == "S":
+                d += 1
+            if nd.tag == "SecCallPathProfileData":
+                return d
+            nd = nd.getparent()
+        return sys.maxsize
+
+
+
+
